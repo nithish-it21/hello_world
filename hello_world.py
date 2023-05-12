74ae4c60b6ad394a29aa0ca654102330853f0094
nithish-it21 <95203481+nithish-it21@users.noreply.github.com>
2023-05-12 09:44:43 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,11 @@
-print("hello world")
+x = 5
+y = "John"
+print(x)
+print(y)
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
